491f14456d626c407d1738c1fbfde94fcc1265e4
Natalja <natali.dorogaya@gmail.com>
2023-10-05 17:41:18 +0200

    last commit

--- app/src/main/java/com/example/herocoroutines/MyApplication.kt
+++ app/src/main/java/com/example/herocoroutines/MyApplication.kt
@@ -8,25 +8,3 @@ import retrofit2.converter.gson.GsonConverterFactory
 @HiltAndroidApp
 class MyApplication:Application()
 
-/*
-class MyApplication:Application() {
-    lateinit var repo: Repository
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-        repo = Repository(getApiClient())
-    }
-
-    private fun getApiClient():Retrofit{
-        return Retrofit.Builder()
-            .baseUrl("https://akabab.github.io/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    companion object{
-        private lateinit var instance:MyApplication
-        fun getApp() = instance
-    }
-}*/
--- app/src/main/java/com/example/herocoroutines/Repository.kt
+++ app/src/main/java/com/example/herocoroutines/Repository.kt
@@ -3,12 +3,6 @@ package com.example.herocoroutines
 import retrofit2.Response
 import retrofit2.Retrofit
 
-/*
-class Repository(client:Retrofit) {
-    private val apiInterface = client.create(ApiInterface::class.java)
-
-    suspend fun getListHeroes():Response<List<Hero>> = apiInterface.getHeroes()
-}*/
 class Repository(private val client:ApiClient) {
     private val apiInterface = client.client.create(ApiInterface::class.java)
 
